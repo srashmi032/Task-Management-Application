ea7148f2b92189bc47377f8e80b70c316dac2a3e
Rashmi <sahurashmi715@gmail.com>
2026-06-10 23:55:10 +0530

    create a dependency for generating and validating jwt token for secured authentication

---
+++ jwt.py
@@ -0,0 +1,22 @@
+import jwt
+from env import SECRET_KEY
+from datetime import datetime, timedelta
+
+async def create_jwt_token(user_id: int):
+    payload = {
+        "user_id": user_id,
+        "exp": datetime.utcnow() + timedelta(hours=24)
+    }
+    token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return token
+
+async def decode_jwt_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        return payload["user_id"]
+    except jwt.ExpiredSignatureError:
+        return "Acess token has expired"
+    except jwt.InvalidTokenError:
+        return "Invalid access token"
+    
+
